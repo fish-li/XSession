c81f84696323a3da39568ba8eb511a5a3e49009d
Fish Li <liqf01@mingyuanyun.com>
2019-05-30 14:22:10 +0800

    更新示例代码

--- XSession.Modules/DemoCode.cs
+++ XSession.Modules/DemoCode.cs
@@ -7,12 +7,28 @@ namespace XSession.Modules
 {
     internal class DemoCode
     {
+        public void 现有代码样例()
+        {
+            // 获取当前用户的SessionId 。  注意：请确保当前请求的Session对象不为NULL
+            string sessionId = System.Web.HttpContext.Current.Session.SessionID;
+
+            // 计算与用户相关的文件，用于读写操作
+            string filePath = $@"c:\temp\xx_{sessionId}.txt";
+
+            // 写文件
+            System.IO.File.WriteAllText(filePath, "xxxxxxxxxxxxx", Encoding.UTF8);
+
+            // 读文件
+            string text = System.IO.File.ReadAllText(filePath, Encoding.UTF8);
+        }
+        
+
         /// <summary>
         /// 演示读写与用户关联的临时文件。
         /// 
         /// 可解决【同一用户】多请求的并发执行问题。
         /// </summary>
-        public void FileReadWrite()
+        public void 调整后的代码()
         {
             // 获取当前用户的SessionId 。  注意：请确保当前请求的Session对象不为NULL
             string sessionId = System.Web.HttpContext.Current.Session.SessionID;
